181bf6aeec7e158a2ef75160e53ef3e223420cfc
Your Name <you@example.com>
2021-02-09 11:52:41 -0800

    Labs from 2/9

--- mangel4.c
+++ mangel4.c
@@ -1,91 +1,23 @@
 //Monika Angel
 //CSC251
-#include <stdio.h>
-#include <stdlib.h>
-
-void chPrint(void);
-
-int prompt(void); //a function prototype
-void printer(int h);
-
-char c;
-//global variable
-
-int main(void)
-{
-    int x, y, z, i;
-
-    printf("Enter a char: \n");
-    scanf(" %c", &c);
-
-    //printf("Enter Number: ");
-    //scanf(" %d", &x);
-
-    x = prompt();
-    y = prompt();
-    z = prompt();
-
-    //printf("Number was %d \n", x);
-    printer(x);
-    printer(y);
-    printer(z);
-
-    chPrint();
-
-    return EXIT_SUCCESS;
-}
-
-int prompt(void)
-{
-    int g;
-
-    printf("Enter a number: ");
-    scanf(" %d", &g);
-
-    return g;
-}
-
-void printer(int h)
-{
-    printf("Your number was %d \n", h);
-}
-
-void chPrint(void)
-{
-    printf("Char is %c \n", c);
-}
-
-/*
-//Monika Angel
-//CSC251
-//2/8/21
+//2/9/21
 //Lab 4
-//Modify Lab #3 by creating separate functions for each task.
-
-//Prompt user to enter grades. You may use global variables only for grade counters but you must pass,
-//to the main function, the total number of grades entered (total grades entered cannot be global variable.)
-//Calculate GPA. You must pass the average back to the main.
-//Print output. You must pass, to this function, the average.
-
-//Your main function should only have function calls and variables. Do not call a function from another function.
-//Submit your source code.
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void printout(int total);
+void printout(double total);
 int prompt(void);
 double calculation(int averageSum);
-//change above
 
 int A = 0, B = 0, C = 0, D = 0, F = 0, true = 1, counter = 0;
 
 int main(void)
 {
-    int averageSum = prompt();
+    double averageSum = prompt();
     //shows prompt
 
-    int GPA = calculation(averageSum);
+    double GPA = calculation(averageSum);
     printout(GPA);
     //since we dont have to grab anything we dont initialize
 
@@ -99,7 +31,6 @@ int prompt(void)
 
     int average = 0;
 
-    
     while (true)
     {
         printf("Enter a letter Grade (press q to quit): ");
@@ -162,7 +93,7 @@ int prompt(void)
     return average;
 }
 
-void printout(int GPA)
+void printout(double GPA)
 {
     //we are getting the global GPA
     printf("There are %d A's, %d B's, %d C's, %d D's, and %d F's \n", A, B, C, D, F);
@@ -175,4 +106,3 @@ double calculation(int averageSum)
     double total = (double)averageSum / (double)counter;
     return total;
 }
-*/
