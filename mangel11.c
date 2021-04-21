6cec4e3d28a163caaa184f2af9a6b0c9e67d1ff7
Monika <monikaangel.ma15@gmail.com>
2021-04-21 16:34:20 -0700

    New project and gitlab

--- mangel11.c
+++ mangel11.c
@@ -1,21 +1,7 @@
-// Create a program that takes 2 arguments from the terminal. The first argument is an
-//infile (which will be a .txt wordlist) and the second argument is an outfile.
-// Prompt the user with 4 options on how to manipulate the wordlist. After the program
-//completes the outfile should contain the modified wordlist.
-
-/* ./l wordlist.txt manipulate.txt
-then itll prompt to what modification to do
-
-Choose an option below to modify your wordlist
-1. Change the second letter to a capital letter.
-2. Make each word lowercase except for the last letter.
-3. Make each word uppercase except for the first letter.
-4. Duplicate each word 5 times and display the word with
-it's duplicate # after it. (word1) one word per line.
-5. Exit program
-
-*/
-// Wordlist complete. Check "outfile.txt" for results.
+//Monika Angel
+//CSC251
+//Lab11
+//04/19/21
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -57,100 +43,100 @@ int main(int argc, char *argv[])
                 //decision 1 - 5
                 switch (choice)
                 {
-                case 1:
-                {
-                    int letterCounter = 1;
-
-                    while ((c = getc(wordlist)) != EOF)
+                    case 1:
                     {
-                        if (c == '\n')
+                        int letterCounter = 1;
+
+                        while ((c = getc(wordlist)) != EOF)
                         {
-                            letterCounter = 0;
+                            if (c == '\n')
+                            {
+                                letterCounter = 0;
+                            }
+                            if (letterCounter == 2)
+                            {
+                                fputc(toupper(c), manipulateToChange);
+                            }
+                            else
+                            {
+                                fputc(c, manipulateToChange);
+                            }
+                            letterCounter++;
                         }
-                        if (letterCounter == 2)
-                        {
-                            fputc(toupper(c), manipulateToChange);
-                        }
-                        else
-                        {
-                            fputc(c, manipulateToChange);
-                        }
-                        letterCounter++;
+                        rewind(wordlist);
+                        fprintf(manipulateToChange, "\n");
+
+                        break;
                     }
-                    rewind(wordlist);
-                    fprintf(manipulateToChange, "\n");
-
-                    break;
-                }
-                case 2:
-                {
-                    char string[25];
-                    int i = 0;
-
-                    while (!feof(wordlist))
-                    // //while ((c = getc(wordlist)) != EOF)
+                    case 2:
                     {
+                        char string[25];
+                        int i = 0;
+
+                        while (!feof(wordlist))
+                        // //while ((c = getc(wordlist)) != EOF)
+                        {
+
+                            fprintf(manipulateToChange, "\n");
+                            fscanf(wordlist, "%s", string);
+
+                            int length = strlen(string);
+
+                            string[length - 1] = toupper(string[length - 1]);
+                            fprintf(manipulateToChange, "%s", string);
+                        }
+                        rewind(wordlist);
+                        break;
+                    }
+                    case 3:
+                    {
+                        fprintf(manipulateToChange, "\n");
+                        fprintf(manipulateToChange, "\n");
+                        int letterCounter = 2;
+
+                        while ((c = getc(wordlist)) != EOF)
+                        {
+                            if (c == '\n')
+                            {
+                                letterCounter = 1;
+                            }
+                            if (letterCounter == 2)
+                            {
+                                fputc(tolower(c), manipulateToChange);
+                            }
+                            else
+                            {
+                                fputc(toupper(c), manipulateToChange);
+                            }
+                            letterCounter++;
+                        }
 
                         fprintf(manipulateToChange, "\n");
-                        fscanf(wordlist, "%s", string);
-
-                        int length = strlen(string);
-
-                        string[length - 1] = toupper(string[length - 1]);
-                        fprintf(manipulateToChange, "%s", string);
+                        rewind(wordlist);
+                        break;
                     }
-                    rewind(wordlist);
-                    break;
-                }
-                case 3:
-                {
-                    fprintf(manipulateToChange, "\n");
-                    fprintf(manipulateToChange, "\n");
-                    int letterCounter = 2;
-
-                    while ((c = getc(wordlist)) != EOF)
+                    case 4:
                     {
-                        if (c == '\n')
+                        int letterCounter = 1;
+                        int i;
+                        char string[25];
+
+                        while (!feof(wordlist))
+                        //while ((c = getc(wordlist)) != EOF)
                         {
-                            letterCounter = 1;
+
+                            fprintf(manipulateToChange, "\n");
+                            fscanf(wordlist, "%s", string);
+
+                            for (i = 1; i <= 5; i++)
+                            {
+                                fprintf(manipulateToChange, "%s%d\n", string, i);
+                            }
                         }
-                        if (letterCounter == 2)
-                        {
-                            fputc(tolower(c), manipulateToChange);
-                        }
-                        else
-                        {
-                            fputc(toupper(c), manipulateToChange);
-                        }
-                        letterCounter++;
+
+                        rewind(wordlist);
+                        break;
                     }
-
-                    fprintf(manipulateToChange, "\n");
-                    rewind(wordlist);
-                    break;
-                }
-                case 4:
-                {
-                    int letterCounter = 1;
-                    int i;
-                    char word[25];
-
-                    while (!feof(wordlist))
-                    //while ((c = getc(wordlist)) != EOF)
-                    {
-
-                        fprintf(manipulateToChange, "\n");
-                        fscanf(wordlist, "%s", word);
-
-                        for (i = 1; i <= 5; i++)
-                        {
-                            fprintf(manipulateToChange, "%s%d\n", word, i);
-                        }
-                    }
-
-                    rewind(wordlist);
-                    break;
-                }
                 }
             }
         }
@@ -158,7 +144,7 @@ int main(int argc, char *argv[])
     fclose(manipulateToChange);
     fclose(wordlist);
 
-    printf("Wordlist complete. Check outfile.txt for results.\n");
+    printf("Wordlist complete. Check %s for results.\n", argv[2]);
 
     return EXIT_SUCCESS;
 }
